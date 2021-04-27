eb29dd6c76e24b78053fcad62652402c140ac66c
Paolo U <paolourciullo@gmail.com>
2021-04-27 10:14:00 -0400

    fix error: fxn args

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
-// console.log('in Javascript');
 $(document).ready(function () {
-  //   console.log('in ready function');
   $('#weather').click(function () {
-    // console.log('in click event');
     var zipcode = $('#zipcode').val();
 
     if (zipcode != '') {
-      //   console.log('before call');
       $.ajax({
         url:
           'https://api.openweathermap.org/data/2.5/weather?zip=' +
           zipcode +
           '&units=imperial' +
           '&appid=b22ef57471e99e0551f0828734a0ad18',
-        // url:"https://api.openweathermap.org/data/2.5/weather?zip=06468&units=imperial&appid=b22ef57471e99e0551f0828734a0ad18",
+
         type: 'GET',
         dataType: 'jsonp',
         success: function (data) {
@@ -30,7 +26,7 @@ $(document).ready(function () {
           $('#temp-max').text(data.main.temp_max);
           $('#humidity').text(data.main.humidity);
         },
-        error: function (xhr, status, error) {
+        error: function (xhr) {
           var errorMessage = xhr.status + ': ' + xhr.statusText;
           alert('Error - ' + errorMessage);
         },
@@ -38,9 +34,3 @@ $(document).ready(function () {
     }
   });
 });
-
-// error: function (xhr, status, error) {
-//     var errorMessage = xhr.status + ' + xhr.responseText';
-//     alert('Error - ' + errorMessage);
-// }
-// }
